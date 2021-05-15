732de5d685c275ef8326f5a7c0b9f917bc1e62e5
Augusto002 <henrique.augustob27@gmail.com>
2021-05-14 23:47:18 -0300

    Adicionar Atividade

---
+++ AtividadePPAugusto/Perguntas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadePPAugusto
+{
+  class Perguntas
+    {
+       public string[] PERG = new string[] {
+            "Qual seleção ganhou a copa de 2018? ", //França 1º
+            "Qual o rei da Selva? ", //Leão 2º
+            "Qual fruto é conhecido no Norte e Nordeste como jerimum?",//Abóbora 3º
+            "Em que lugar vivem mais cangurus do que pessoas?",//Austrália 4º
+            "Quantos olhos a maior parte das aranhas têm?",//Quatro pares 5º
+            };
+    }
+}
---
+++ AtividadePPAugusto/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace AtividadePPAugusto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            string nome;
+            int ponto = 0;
+            int nivel = 0;
+
+
+            Console.WriteLine("Infomre seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("************VAMOS COMEÇAR**************" +
+                "\n**************1º NIVEL:***************");
+
+             Perguntas ler; 
+             ler = new Perguntas();
+
+            for (int i = 0; i <= ler.PERG.Length; i++)
+            {
+                nivel = +i;
+                if (nivel == 0)
+                {
+                    Console.WriteLine(ler.PERG[nivel] +
+                        "\nA)Brasil \n" +
+                        "B)França \n" +
+                        "C)Alemanhã \n" +
+                        "D)Argentina \n" +
+                        "[SEU NOME:] " + nome +
+                        "\n[Pontuação:] " + ponto +
+                        "\n[Nivel:] " + nivel);
+                    String resposta = Console.ReadLine();
+                    switch (resposta)
+                    {
+                        case "B":
+                            Console.WriteLine("Você Acertou ");
+                            ponto = ponto + 10;
+                            break;
+                        case "A":
+                            Console.WriteLine("Você Errou ");
+                            ponto = ponto - ponto;
+                            break;
+                        case "C":
+                            Console.WriteLine("Você Errou ");
+                            ponto = ponto - ponto;
+                            break;
+                        case "D":
+                            Console.WriteLine("Você Errou ");
+                            ponto = ponto - ponto;
+                            break;
+
+
+                    }
+
+
+                }
+                else if (nivel == 1)
+                {
+                    Console.WriteLine(ler.PERG[nivel] +
+                           "\nA)Galinha \n" +
+                           "B)Zebra \n" +
+                           "C)Macaco \n" +
+                           "D)Leão \n" +
+                           "[SEU NOME:] " + nome +
+                           "\n[Pontuação:] " + ponto +
+                           "\n[Nivel:] " + nivel);
+                    String resposta = Console.ReadLine();
+                    switch (resposta)
+                    {
+                        case "D":
+                            Console.WriteLine("Você Acertou ");
+                            ponto = ponto + 30;
+                            break;
+                        case "A":
+                            Console.WriteLine("Você Errou ");
+                            ponto = ponto - ponto;
+                            break;
+                        case "C":
+                            Console.WriteLine("Você Errou ");
+                            ponto = ponto - ponto;
+                            break;
+                        case "B":
+                            Console.WriteLine("Você Errou ");
+                            ponto = ponto - ponto;
+                            break;
+                    }
+                } else if (nivel==2) 
+                {
+                    Console.WriteLine(ler.PERG[nivel] +
+                            "\nA)Morango \n" +
+                            "B)Abobora \n" +
+                            "C)Castanha \n" +
+                            "D)Limão \n" +
+                            "[SEU NOME:] " + nome +
+                            "\n[Pontuação:] " + ponto +
+                            "\n[Nivel:] " + nivel);
+                    String resposta = Console.ReadLine();
+                    switch (resposta)
+                    {
+                        case "B":
+                            Console.WriteLine("Você Acertou ");
+                            ponto = ponto + 13;
+                            break;
+                        case "A":
+                            Console.WriteLine("Você Errou ");
+                            ponto = ponto - ponto;
+                            break;
+                        case "C":
+                            Console.WriteLine("Você Errou ");
+                            ponto = ponto - ponto;
+                            break;
+                        case "D":
+                            Console.WriteLine("Você Errou ");
+                            ponto = ponto - ponto;
+                            break;
+                    }
+                }
+            }
+
+
+     
+           
+
+
+
+
+        }
+
+      
+
+    }
+}
